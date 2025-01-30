10139cf0718f544d1634a0c7457a836b24d0f0d5
Zainabu111 <ndizeyezai111@gmail.com>
2025-01-30 06:13:12 -0800

    commit 1

---
+++ Pyschologie.php
@@ -0,0 +1,18 @@
+<?php
+include('header.php');
+?>
+<main>
+    <div class="container-fluid px-4">
+        <h4 class="mt-4">Liste des étudiants de Pyschologie clinique</h4>
+                    
+
+
+
+
+
+    </div>                        
+</main>
+<?php
+include('footer.php');
+?>
+
---
+++ civil.php
@@ -0,0 +1,18 @@
+<?php
+include('header.php');
+?>
+<main>
+    <div class="container-fluid px-4">
+        <h4 class="mt-4">Liste des étudiants de Génie Civil</h4>
+                    
+
+
+
+
+
+    </div>                        
+</main>
+<?php
+include('footer.php');
+?>
+
